37ab10b6c1acaff1179a197862110cfadbfb3d8f
ShiJingFeng <1634886078@qq.com>
2021-05-16 17:20:53 +0800

    1.0.0版本优化

--- app/src/main/java/com/shijingfeng/module_event_dispatcher_apt_processor/MainActivity.kt
+++ app/src/main/java/com/shijingfeng/module_event_dispatcher_apt_processor/MainActivity.kt
@@ -2,6 +2,7 @@ package com.shijingfeng.module_event_dispatcher_apt_processor
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.shijingfeng.module_event_dispatcher.data.annotations.ModuleEventReceiver
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
